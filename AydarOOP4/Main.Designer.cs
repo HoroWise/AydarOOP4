25b80d6641ad81ccf4ef5126ca3ea7f0c8c6726d
HoroWise <79516164+HoroWise@users.noreply.github.com>
2021-04-08 22:56:17 +0500

    Добавьте файлы проекта.

---
+++ AydarOOP4/Main.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AydarOOP4
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paint_box = new System.Windows.Forms.Panel();
+            this.label_paintbox = new System.Windows.Forms.Label();
+            this.label_x = new System.Windows.Forms.Label();
+            this.label_y = new System.Windows.Forms.Label();
+            this.button_clear_paintbox = new System.Windows.Forms.Button();
+            this.button_show = new System.Windows.Forms.Button();
+            this.button_deletestorage = new System.Windows.Forms.Button();
+            this.button_del_item_storage = new System.Windows.Forms.Button();
+            this.paint_box.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // paint_box
+            // 
+            this.paint_box.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.paint_box.BackColor = System.Drawing.SystemColors.Info;
+            this.paint_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paint_box.Controls.Add(this.label_paintbox);
+            this.paint_box.Location = new System.Drawing.Point(9, 10);
+            this.paint_box.Margin = new System.Windows.Forms.Padding(2);
+            this.paint_box.Name = "paint_box";
+            this.paint_box.Size = new System.Drawing.Size(454, 388);
+            this.paint_box.TabIndex = 0;
+            this.paint_box.MouseClick += new System.Windows.Forms.MouseEventHandler(this.paint_box_MouseClick);
+            this.paint_box.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paint_box_MouseMove);
+            // 
+            // label_paintbox
+            // 
+            this.label_paintbox.AutoSize = true;
+            this.label_paintbox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_paintbox.Location = new System.Drawing.Point(3, 3);
+            this.label_paintbox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_paintbox.Name = "label_paintbox";
+            this.label_paintbox.Size = new System.Drawing.Size(157, 21);
+            this.label_paintbox.TabIndex = 0;
+            this.label_paintbox.Text = "Окно для рисования";
+            // 
+            // label_x
+            // 
+            this.label_x.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_x.AutoSize = true;
+            this.label_x.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label_x.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_x.Location = new System.Drawing.Point(467, 10);
+            this.label_x.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_x.Name = "label_x";
+            this.label_x.Size = new System.Drawing.Size(119, 21);
+            this.label_x.TabIndex = 1;
+            this.label_x.Text = "Координаты X: ";
+            // 
+            // label_y
+            // 
+            this.label_y.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_y.AutoSize = true;
+            this.label_y.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label_y.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_y.Location = new System.Drawing.Point(467, 32);
+            this.label_y.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_y.Name = "label_y";
+            this.label_y.Size = new System.Drawing.Size(119, 21);
+            this.label_y.TabIndex = 2;
+            this.label_y.Text = "Координаты Y: ";
+            // 
+            // button_clear_paintbox
+            // 
+            this.button_clear_paintbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_clear_paintbox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_clear_paintbox.Location = new System.Drawing.Point(471, 352);
+            this.button_clear_paintbox.Margin = new System.Windows.Forms.Padding(2);
+            this.button_clear_paintbox.Name = "button_clear_paintbox";
+            this.button_clear_paintbox.Size = new System.Drawing.Size(134, 46);
+            this.button_clear_paintbox.TabIndex = 3;
+            this.button_clear_paintbox.Text = "Очистить панель";
+            this.button_clear_paintbox.UseVisualStyleBackColor = true;
+            this.button_clear_paintbox.Click += new System.EventHandler(this.button_clear_paintbox_Click);
+            // 
+            // button_show
+            // 
+            this.button_show.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_show.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_show.Location = new System.Drawing.Point(471, 58);
+            this.button_show.Margin = new System.Windows.Forms.Padding(2);
+            this.button_show.Name = "button_show";
+            this.button_show.Size = new System.Drawing.Size(134, 79);
+            this.button_show.TabIndex = 4;
+            this.button_show.Text = "Отобразить объекты из хранилища";
+            this.button_show.UseVisualStyleBackColor = true;
+            this.button_show.Click += new System.EventHandler(this.button_show_Click);
+            // 
+            // button_deletestorage
+            // 
+            this.button_deletestorage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_deletestorage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_deletestorage.Location = new System.Drawing.Point(471, 268);
+            this.button_deletestorage.Margin = new System.Windows.Forms.Padding(2);
+            this.button_deletestorage.Name = "button_deletestorage";
+            this.button_deletestorage.Size = new System.Drawing.Size(134, 79);
+            this.button_deletestorage.TabIndex = 5;
+            this.button_deletestorage.Text = "Удалить все элементы из хранилища";
+            this.button_deletestorage.UseVisualStyleBackColor = true;
+            this.button_deletestorage.Click += new System.EventHandler(this.button_deletestorage_Click);
+            // 
+            // button_del_item_storage
+            // 
+            this.button_del_item_storage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_del_item_storage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_del_item_storage.Location = new System.Drawing.Point(471, 141);
+            this.button_del_item_storage.Margin = new System.Windows.Forms.Padding(2);
+            this.button_del_item_storage.Name = "button_del_item_storage";
+            this.button_del_item_storage.Size = new System.Drawing.Size(134, 122);
+            this.button_del_item_storage.TabIndex = 6;
+            this.button_del_item_storage.Text = "Удалить выделенные элементы из хранилища и панели";
+            this.button_del_item_storage.UseVisualStyleBackColor = true;
+            this.button_del_item_storage.Click += new System.EventHandler(this.button_del_item_storage_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 407);
+            this.Controls.Add(this.button_del_item_storage);
+            this.Controls.Add(this.button_clear_paintbox);
+            this.Controls.Add(this.button_deletestorage);
+            this.Controls.Add(this.button_show);
+            this.Controls.Add(this.label_y);
+            this.Controls.Add(this.label_x);
+            this.Controls.Add(this.paint_box);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Main";
+            this.Text = "AydarOOP4";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.paint_box.ResumeLayout(false);
+            this.paint_box.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel paint_box;
+        private System.Windows.Forms.Label label_paintbox;
+        private System.Windows.Forms.Label label_x;
+        private System.Windows.Forms.Label label_y;
+        private System.Windows.Forms.Button button_clear_paintbox;
+        private System.Windows.Forms.Button button_show;
+        private System.Windows.Forms.Button button_deletestorage;
+        private System.Windows.Forms.Button button_del_item_storage;
+    }
+}
+
---
+++ AydarOOP4/Main.cs
@@ -0,0 +1,305 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AydarOOP4
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        class Circle
+        {
+            protected int x, y; // Координаты круга
+            protected int rad = 30; // Радиус круга
+            protected Color color = Color.Navy; // Выделен ли элемент
+            protected bool is_drawed = true; // Нарисован ли круг на полотне
+            public Circle()
+            {
+                x = 0;
+                y = 0;
+            }
+            public Circle(int x, int y)
+            {
+                this.x = x-rad;
+                this.y = y-rad;
+            }
+            public void paint_figure(Pen pen, Panel panel_drawing)
+            {   // Отображение фигуры
+                panel_drawing.CreateGraphics().DrawEllipse(
+                    pen, x, y, rad * 2, rad * 2);
+            }
+            public void setcolor(Color color)
+            {   // Установка выделения
+                this.color = color;
+            }
+            public bool checkfigure(int x, int y)
+            {   // Проверка на фигуры
+                return ((x - this.x - rad) * (x - this.x - rad) + (y - this.y - rad) *
+                    (y - this.y - rad)) < (rad * rad);
+            }
+            public string getcolor()
+            {
+                return color.ToArgb().ToString();
+            }
+            public bool checkDrawed()
+            {   // Проверка на фигуры
+                return is_drawed;
+            }
+            public void setDrawed(bool is_drawed)
+            {   // Проверка на фигуры
+                this.is_drawed = is_drawed;
+            }
+            ~Circle() { }
+        }
+
+        private void paint_box_MouseMove(object sender, MouseEventArgs e)
+        {
+            label_x.Text = "Координаты X: " + e.X.ToString();
+            label_y.Text = "Координаты Y: " + e.Y.ToString();
+        }
+
+        private void paint_circle(Color name, ref Storage stg, int index)
+        {   // Рисует круг на панели          
+            // Объявляем объект - карандаш, которым будем рисовать контур
+            if (!stg.check_empty(index))
+            {
+                Pen pen = new Pen(name, 3);
+                stg.get(index).setcolor(name);
+                stg.get(index).paint_figure(pen, paint_box);
+            }
+        }
+
+        private void remove_selection_circle(ref Storage stg)
+        {   // Снимает выделение у всех элементов хранилища
+            for (int i = 0; i < k; ++i)
+            {
+                if (!storag.check_empty(i))
+                {   // Вызываем функцию отрисовки круга
+                    paint_circle(Color.Navy, ref storag, i);
+                }
+            }
+        }
+
+        private void remove_selected_circle(ref Storage stg)
+        {   // Удаляет выделенные элементы из хранилища
+            for (int i = 0; i < k; ++i)
+            {
+                if (!storag.check_empty(i))
+                {   
+                    if(storag.get(i).getcolor() == Color.Red.ToArgb().ToString())
+                    {
+                        storag.delete_object(i);
+                    }
+                }
+            }
+        }
+
+        private void Main_MouseMove(object sender, MouseEventArgs e)
+        {
+            label_x.Text = "";
+            label_y.Text = "";
+        }
+
+        int p = 0; // Нажат ли был ранее Ctrl
+        static int k = 5; // Кол-во ячеек в хранилище
+        Storage storag = new Storage(k); // Создаем объект хранилища
+        static int index = 0; // Кол-во нарисованных кругов
+        int indexin = 0; // Индекс, в какое место был помещён круг
+
+        private void paint_box_MouseClick(object sender, MouseEventArgs e)
+        {
+            Circle krug = new Circle(e.X, e.Y);
+            if (index == k)
+                storag.doubleSize(ref k);
+            // Проверка на наличие круга на данных координатах
+            int c = check_circle(ref storag, k, e.X, e.Y);
+            // Если на этом месте уже нарисован круг
+            if (c != -1)
+            {   // Если нажат ctrl, то выделяем несколько объектов
+                if (Control.ModifierKeys == Keys.Control)  
+                {   
+                    if (p == 0)
+                    {
+                        //paint_circle(Color.Navy, ref storag, indexin);
+                        p = 1;
+                    }
+                    // Вызываем функцию отрисовки круга
+                        paint_circle(Color.Red, ref storag, c);
+                }
+                else
+                {   // Иначе выделяем только один объект
+                    // Снимаем выделение у всех объектов хранилища
+                    remove_selection_circle(ref storag); 
+                    // Вызываем функцию отрисовки круга
+                    paint_circle(Color.Red, ref storag, c);
+                }
+                return;
+            }
+            // Добавляем круг в хранилище   
+            storag.add_object(index, ref krug, k, ref indexin);
+            // Снимаем выделение у всех объектов хранилища
+            remove_selection_circle(ref storag);
+
+            // Вызываем функцию отрисовки круга
+            paint_circle(Color.Red, ref storag, indexin);
+            //label_paintbox.Visible = false;
+            ++index;
+            p = 0;
+        }
+
+        private int check_circle(ref Storage stg, int size, int x, int y)
+        {   // Проверяет есть ли уже круг с такими же координатами в хранилище
+            if (stg.occupied(size) != 0)
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    if (!stg.check_empty(i))
+                    {   // Если под i индексом в хранилище есть объект
+                        if (stg.get(i).checkfigure(x, y))
+                            return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private void button_clear_paintbox_Click(object sender, EventArgs e)
+        {   // Очищает панель от кругов
+            paint_box.Refresh(); // Перерисовывем панель paint_box
+            //label_paintbox.Visible = true;
+            for (int i = 0; i < k; ++i)
+            {
+                if (!storag.check_empty(i))
+                {   // Меняем is_drawed на false
+                    storag.get(i).setDrawed(false);
+                }
+            }
+        }
+
+        class Storage
+        {
+            protected Circle[] objects;
+
+            public Storage(int count)
+            {   // Конструктор по умолчанию 
+                // Выделяем count мест в хранилище
+                objects = new Circle[count];
+                for (int i = 0; i < count; ++i)
+                    objects[i] = null;
+            }
+
+            public Circle get(int index)
+            {
+                return objects[index];
+            }
+            public void initialize(int count)
+            {   // Выделяем count мест в хранилище
+                objects = new Circle[count];
+                for (int i = 0; i < count; ++i)
+                    objects[i] = null;
+            }
+            public void add_object(int ind, ref Circle object1, int count, ref int indexin)
+            {   // Добавляет ячейку в хранилище
+                // Если ячейка занята ищем свободное место
+                while (objects[ind] != null)
+                {
+                    ind = (ind + 1) % count;                   
+                }
+                objects[ind] = object1;
+                indexin = ind;
+            }
+
+            public void delete_object(int ind)
+            {   // Удаляет объект из хранилища
+                objects[ind] = null;
+                index--;
+            }
+
+            public bool check_empty(int index)
+            {   // Проверяет занято ли место хранилище
+                if (objects[index] == null)
+                    return true;
+                else return false;
+            }
+
+            public int occupied(int size)
+            { // Определяет кол-во занятых мест в хранилище
+                int count_occupied = 0;
+                for (int i = 0; i < size; ++i)
+                    if (!check_empty(i))
+                        ++count_occupied;
+                return count_occupied;
+            }
+
+            public void doubleSize(ref int size)
+            {   // Функция для увеличения кол-ва элементов в хранилище в 2 раза 
+                Storage storage1 = new Storage(size * 2);
+                for (int i = 0; i < size; ++i)
+                    storage1.objects[i] = objects[i];
+                for (int i = size; i < (size * 2) - 1; ++i)
+                {
+                    storage1.objects[i] = null;
+                }
+                size = size * 2;
+                initialize(size);
+                for (int i = 0; i < size; ++i)
+                    objects[i] = storage1.objects[i];
+            }
+
+            ~Storage() { }
+        };
+
+        private void button_show_Click(object sender, EventArgs e)
+        {   // Отобразить все круги из хранилища
+            paint_box.Refresh();
+            if (storag.occupied(k) != 0)
+            {
+                //label_paintbox.Visible = false;
+                for (int i = 0; i < k; ++i)
+                {
+                    if (!storag.check_empty(i))
+                    {   // Меняем is_drawed на true
+                        storag.get(i).setDrawed(true);
+                    }
+                    paint_circle(Color.Navy, ref storag, i);
+                }
+            }
+
+        }
+
+        private void button_deletestorage_Click(object sender, EventArgs e)
+        {   // Удалить все круги из хранилища
+            for (int i = 0; i < k; ++i)
+            {
+                storag.delete_object(i);
+            }
+            index = 0;
+            paint_box.Refresh();
+        }
+
+        private void button_del_item_storage_Click(object sender, EventArgs e)
+        {   // Обработчик на удаление выделенных элементов из хранилища
+            remove_selected_circle(ref storag);
+            paint_box.Refresh();
+            if (storag.occupied(k) != 0)
+            {
+                for (int i = 0; i < k; ++i)
+                {
+                    paint_circle(Color.Navy, ref storag, i);
+                }
+            }
+        }
+    }
+
+}
+
